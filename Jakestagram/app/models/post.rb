2a1f834265934f5a462ea452877bf8fafbde07f3
Jake Napper <napperjlg@gmail.com>
2019-05-27 20:33:13 +0100

    add user signup/login with devise gem

--- Jakestagram/app/models/post.rb
+++ Jakestagram/app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+belongs_to :user
 has_one_attached :post_picture
 has_many :comments
+has_many :posts
 
 def gallery_view
   return self.post_picture.variant(resize: '300x300').processed
